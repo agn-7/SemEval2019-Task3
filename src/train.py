179a00e36ab3ccadc1280d8dd844faf5f900bbe2
Hussem Ben Belgacem <hussem.benbelgacem@gmail.com>
2018-12-27 20:43:19 +0100

    [MODELS] Adding custom metrics + direct computation of f1 score without send

--- src/train.py
+++ src/train.py
@@ -14,11 +14,22 @@ from sklearn.model_selection import KFold
 from keras.layers import Input, Dense, Embedding, LSTM, Concatenate, Reshape, GRU, Bidirectional, Dropout, Conv1D, Flatten, MaxPool1D, TimeDistributed
 from keras.models import Sequential
 from keras import optimizers
-
-
+from keras.callbacks import Callback
+from sklearn.model_selection import train_test_split
 np.random.seed(7)
 
 
+class TestCallback(Callback):
+    def __init__(self, test_data):
+        self.test_data = test_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        x, y = self.test_data
+        r = self.model.predict(x)
+        getMetrics(r, y)
+
+
+validationDataPath = ""
 trainDataPath = ""
 testDataPath = ""
 solutionPath = ""
@@ -40,10 +51,11 @@ def load_config(configPath):
     with open(configPath) as configfile:
         config = json.load(configfile)
 
-    global trainDataPath, testDataPath, solutionPath, gloveDir
+    global validationDataPath, trainDataPath, testDataPath, solutionPath, gloveDir
     global NUM_FOLDS, NUM_CLASSES, MAX_NB_WORDS, EMBEDDING_DIM
     global BATCH_SIZE, LSTM_DIM, DROPOUT, NUM_EPOCHS, LEARNING_RATE, MAX_SEQUENCE_LENGTH
-
+    
+    validationDataPath = config["validation_data_path"]
     trainDataPath = config["train_data_path"]
     testDataPath = config["test_data_path"]
     solutionPath = config["solution_path"]
@@ -103,12 +115,73 @@ def getEmbeddingMatrix(wordIndex):
     return embeddingMatrix
 
 
+def getMetrics(predictions, ground):
+    """Given predicted labels and the respective ground truth labels, display some metrics
+    Input: shape [# of samples, NUM_CLASSES]
+        predictions : Model output. Every row has 4 decimal values, with the highest belonging to the predicted class
+        ground : Ground truth labels, converted to one-hot encodings. A sample belonging to Happy class will be [0, 1, 0, 0]
+    Output:
+        accuracy : Average accuracy
+        microPrecision : Precision calculated on a micro level. Ref - https://datascience.stackexchange.com/questions/15989/micro-average-vs-macro-average-performance-in-a-multiclass-classification-settin/16001
+        microRecall : Recall calculated on a micro level
+        microF1 : Harmonic mean of microPrecision and microRecall. Higher value implies better classification  
+    """
+    # [0.1, 0.3 , 0.2, 0.1] -> [0, 1, 0, 0]
+    discretePredictions = to_categorical(predictions.argmax(axis=1))
+    
+    truePositives = np.sum(discretePredictions*ground, axis=0)
+    falsePositives = np.sum(np.clip(discretePredictions - ground, 0, 1), axis=0)
+    falseNegatives = np.sum(np.clip(ground-discretePredictions, 0, 1), axis=0)
+    
+    print("True Positives per class : ", truePositives)
+    print("False Positives per class : ", falsePositives)
+    print("False Negatives per class : ", falseNegatives)
+    
+    # ------------- Macro level calculation ---------------
+    macroPrecision = 0
+    macroRecall = 0
+    # We ignore the "Others" class during the calculation of Precision, Recall and F1
+    for c in range(1, NUM_CLASSES):
+        precision = truePositives[c] / (truePositives[c] + falsePositives[c])
+        macroPrecision += precision
+        recall = truePositives[c] / (truePositives[c] + falseNegatives[c])
+        macroRecall += recall
+        f1 = ( 2 * recall * precision ) / (precision + recall) if (precision+recall) > 0 else 0
+        print("Class %s : Precision : %.3f, Recall : %.3f, F1 : %.3f" % (label2emotion[c], precision, recall, f1))
+    
+    macroPrecision /= 3
+    macroRecall /= 3
+    macroF1 = (2 * macroRecall * macroPrecision ) / (macroPrecision + macroRecall) if (macroPrecision+macroRecall) > 0 else 0
+    print("Ignoring the Others class, Macro Precision : %.4f, Macro Recall : %.4f, Macro F1 : %.4f" % (macroPrecision, macroRecall, macroF1))   
+    
+    # ------------- Micro level calculation ---------------
+    truePositives = truePositives[1:].sum()
+    falsePositives = falsePositives[1:].sum()
+    falseNegatives = falseNegatives[1:].sum()    
+    
+    print("Ignoring the Others class, Micro TP : %d, FP : %d, FN : %d" % (truePositives, falsePositives, falseNegatives))
+    
+    microPrecision = truePositives / (truePositives + falsePositives)
+    microRecall = truePositives / (truePositives + falseNegatives)
+    
+    microF1 = ( 2 * microRecall * microPrecision ) / (microPrecision + microRecall) if (microPrecision+microRecall) > 0 else 0
+    # -----------------------------------------------------
+    
+    predictions = predictions.argmax(axis=1)
+    ground = ground.argmax(axis=1)
+    accuracy = np.mean(predictions==ground)
+    
+    print("Accuracy : %.4f, Micro Precision : %.4f, Micro Recall : %.4f, Micro F1 : %.4f" % (accuracy, microPrecision, microRecall, microF1))
+    
+    return accuracy, microPrecision, microRecall, microF1
+
+
 def model1(embeddingMatrix):
     embeddingLayer = Embedding(embeddingMatrix.shape[0],
                                 EMBEDDING_DIM,
                                 weights=[embeddingMatrix],
                                 input_length=MAX_SEQUENCE_LENGTH,
-                                trainable=False)
+                                trainable=True)
     model = Sequential()
     model.add(embeddingLayer)
     model.add(Dropout(DROPOUT))
@@ -150,7 +223,7 @@ def model3(embeddingMatrix):
                                 EMBEDDING_DIM,
                                 weights=[embeddingMatrix],
                                 input_length=MAX_SEQUENCE_LENGTH,
-                                trainable=False)
+                                trainable=True)
     model = Sequential()
     model.add(embeddingLayer)
     model.add(Dropout(DROPOUT))
@@ -288,12 +361,15 @@ def main():
     trainIndices, text_train, labels = load_preprocessed_data(trainDataPath)
     print("Processing test data...")
     _, text_test = load_preprocessed_data(testDataPath, training=False)
+    print("Processing validation data...")
+    _, X_validation, y_validation = load_preprocessed_data(validationDataPath)
 
     print("Extracting tokens...")
     tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
     tokenizer.fit_on_texts(text_train)
     u_trainSequences = tokenizer.texts_to_sequences(text_train)
     u_testSequences = tokenizer.texts_to_sequences(text_test)
+    u_validationSequences = tokenizer.texts_to_sequences(X_validation)
 
     wordIndex = tokenizer.word_index
     print("Found %s unique tokens." % len(wordIndex))
@@ -302,6 +378,8 @@ def main():
     embeddingMatrix = getEmbeddingMatrix(wordIndex)
     u_data = pad_sequences(u_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
     labels = to_categorical(np.asarray(labels))
+    u_validation = pad_sequences(u_validationSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    labels_validation = to_categorical(np.asarray(y_validation))
 
     np.random.shuffle(trainIndices)
     u_data = u_data[trainIndices]
@@ -309,9 +387,10 @@ def main():
 
     print("Building model...")
     cbks = [ModelCheckpoint('./model1.h5', verbose=1, monitor='val_loss', save_best_only=True, mode='auto'),
-            EarlyStopping(monitor='val_loss', patience=10)]
-    model = model8(embeddingMatrix)
-    model.fit(u_data, labels, validation_split=0.1, epochs=200, batch_size=10, shuffle=True, callbacks=cbks)
+            EarlyStopping(monitor='val_loss', patience=2),
+            TestCallback((u_validation, labels_validation))]
+    model = model4(embeddingMatrix)
+    model.fit(u_data, labels, validation_data=(u_validation, labels_validation), epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, shuffle=True, callbacks=cbks)
     model = load_model('./model1.h5')
     print("Creating solution file...")
     create_solution_file(model, u_testSequences)
